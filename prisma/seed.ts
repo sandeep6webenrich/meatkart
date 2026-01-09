06e7181a5738defeefd8fc068307efe357ec8a3b
Sandeep <sandeep6boddu6@gmail.com>
2026-01-08 21:24:13 -0500

    Update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({ datasourceUrl: process.env.DIRECT_URL });
 
 async function main() {
   console.log('Start seeding ...');
